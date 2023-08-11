0235d666b45534fbfdc0f97b759d4a411c7a7146
Dani Ponce Mendoza <danniscreamer@gmail.com>
2023-08-10 22:11:02 -0600

    Se quitó el inaitorizado por No puedes pasar dentro del auth

--- services/auth.js
+++ services/auth.js
@@ -7,13 +7,13 @@ const authService = class{
 
     async login(email,password){
         const user = await this.UserService.getByEmail(email)
-        
+
         if(!user){
           throw new Error(`Este usuario no existe`)
       } else if(await bcrypt.compare(password, user.password) || !user){
           return user.toObject();
       } else {
-          throw new Error('Inautorizado')
+          throw new Error('No puedes pasar')
       }
     }
 
